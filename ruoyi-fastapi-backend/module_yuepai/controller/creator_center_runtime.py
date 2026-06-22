737d3b8e8614330a6e16ee01f73c9f2022e66231
lizhenwei <62752415+Colouful@users.noreply.github.com>
2026-06-22 11:12:44 +0800

    feat: 创作者中心增加我的资料与内容查询

--- ruoyi-fastapi-backend/module_yuepai/controller/creator_center_runtime.py
+++ ruoyi-fastapi-backend/module_yuepai/controller/creator_center_runtime.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import Response
+from fastapi import Path, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from common.aspect.db_seesion import DBSessionDependency
@@ -10,6 +10,23 @@ from module_admin.entity.vo.user_vo import CurrentUserModel
 from module_yuepai.entity.vo.creator_vo import CreatorProfileUpsert, CreatorWorkCreate, ServicePackageCreate
 from module_yuepai.service.creator_manage_service import CreatorManageService
 from utils.response_util import ResponseUtil
+
+
+async def my_profiles(
+    query_db: Annotated[AsyncSession, DBSessionDependency()],
+    current_user: Annotated[CurrentUserModel, CurrentUserDependency()],
+) -> Response:
+    rows = await CreatorManageService.my_profiles(query_db, current_user.user.user_id)
+    return ResponseUtil.success(rows=rows)
+
+
+async def my_content(
+    creator_id: Annotated[int, Path(ge=1)],
+    query_db: Annotated[AsyncSession, DBSessionDependency()],
+    current_user: Annotated[CurrentUserModel, CurrentUserDependency()],
+) -> Response:
+    data = await CreatorManageService.my_content(query_db, current_user.user.user_id, creator_id)
+    return ResponseUtil.success(data=data)
 
 
 async def save_profile(
@@ -40,6 +57,8 @@ async def create_package(
 
 
 creator_center_runtime = APIRouterPro(prefix='/yuepai/creator-center', order_num=43, tags=['91约拍Pro-创作者中心'])
+creator_center_runtime.add_api_route('/profiles', my_profiles, methods=['GET'], dependencies=[PreAuthDependency()])
+creator_center_runtime.add_api_route('/profiles/{creator_id}/content', my_content, methods=['GET'], dependencies=[PreAuthDependency()])
 creator_center_runtime.add_api_route('/profile', save_profile, methods=['PUT'], dependencies=[PreAuthDependency()])
 creator_center_runtime.add_api_route('/works', create_work, methods=['POST'], dependencies=[PreAuthDependency()])
 creator_center_runtime.add_api_route('/packages', create_package, methods=['POST'], dependencies=[PreAuthDependency()])
